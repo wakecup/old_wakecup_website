3f9057560570545dbd924d67d72dbfdabc7a9179
bielmaia <gab.maia.c@gmail.com>
2017-07-31 13:45:58 -0300

    Update 31-07-17 - Página em construção e contato

--- js/main.js
+++ js/main.js
@@ -1,40 +1,40 @@
-
+/*global $ */
 $(document).ready(function () {
     $("#portfolio-contant-active").mixItUp();
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,
-	    singleItem: true,
-	    autoPlay: 3000
-	});
+    $("#testimonial-slider").owlCarousel({
+        paginationSpeed: 500,
+        singleItem: true,
+        autoPlay: 3000
+    });
 
 
 
 
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199, 5],
-		itemsDesktopSmall : [979, 5]
-	});
+    $("#clients-logo").owlCarousel({
+        autoPlay: 3000,
+        items: 5,
+        itemsDesktop: [1199, 5],
+        itemsDesktopSmall: [979, 5]
+    });
 
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199, 5],
-		itemsDesktopSmall : [979, 5]
-	});
+    $("#works-logo").owlCarousel({
+        autoPlay: 3000,
+        items: 5,
+        itemsDesktop: [1199, 5],
+        itemsDesktopSmall: [979, 5]
+    });
 
 
-	// google map
+// google map
     var map;
-	function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
+    function initMap() {
+        map = new google.maps.Map(document.getElementById("map"), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 8
         });
     }
-	// Counter
-	$('.counter').counterUp({
+    // Counter
+    $('.counter').counterUp({
         delay: 10,
         time: 1000
     });
